f8c2dd534f68f4b4b02fd143369111b2f3daaeab
TerraRoot <TerraRoot@gmail.com>
2024-03-20 12:39:28 +0000

    Delete CVEscrapper-database.sh

--- CVEscrapper-database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#       Security search for CVE's in a file
-cd resolved-database
-# remove uniq as it seems to remove whole lines?
-awk 'match($0,/CVE-[0-9-]+/){print substr($0,RSTART,RLENGTH)}' *.txt | sort | uniq > resolvedv2.txt
-#awk 'match($0,/CVE-[0-9-]+/){print substr($0,RSTART,RLENGTH)}' *.txt | sort > resolvedv2.txt
-
-cp resolvedv2.txt ..
